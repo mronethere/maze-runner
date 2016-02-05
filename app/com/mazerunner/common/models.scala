50f6b413e89e9c33b3dec05e916faf04f2470148
mentegy <mentegy@gmail.com>
2016-02-06 01:08:31 +0200

    Common package:  - remove package.scala  - add models.scala

---
+++ app/com/mazerunner/common/models.scala
@@ -0,0 +1,7 @@
+package com.mazerunner.common
+
+
+object models {
+  case class Credentials(username: String, password: String)
+  case class User(username: String, password: String)
+}
--- app/com/mazerunner/common/package.scala
+++
@@ -1,8 +0,0 @@
-package com.mazerunner
-
-import play.api.libs.json.Json
-import play.api.mvc.{AnyContent, Request}
-
-package object common {
-  case class Credentials(username: String, password: String)
-}

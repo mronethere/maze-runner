d123051714420da09443abe6aa9aa89ff323c942
mentegy <mentegy@gmail.com>
2016-02-08 14:50:47 +0200

    Add several loggers

--- app/mazerunner/common/ControllerUtils.scala
+++ app/mazerunner/common/ControllerUtils.scala
@@ -1,5 +1,6 @@
 package mazerunner.common
 
+import play.api.Logger
 import play.api.libs.json.JsResult
 import play.api.mvc.{Result, Controller}
 
@@ -8,9 +9,14 @@ import scala.concurrent.Future
 trait ControllerUtils {
   self: Controller =>
 
+  val logger: Logger = Logger(this.getClass)
+
   def transformAsFuture[T](json: JsResult[T]): Future[T] =
     if (json.isSuccess) Future.successful(json.get)
-    else Future.failed(new Exception("error parsing json"))
+    else {
+      logger.debug(s"unable to parse json value: $json")
+      Future.failed(new Exception("error parsing json"))
+    }
 
   val withRecover: PartialFunction[Throwable, Result] = {
     case e: Exception => BadRequest(s"bad request: ${e.getMessage}")
